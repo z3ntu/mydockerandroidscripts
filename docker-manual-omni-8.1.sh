aa9439ef10e877007ac28a9b0dd42cd6eac0d3f7
Luca Weiss <luca@z3ntu.xyz>
2019-09-25 14:32:08 +0200

    Add flag to omni-8.1

--- docker-manual-omni-8.1.sh
+++ docker-manual-omni-8.1.sh
@@ -5,6 +5,7 @@ source $(dirname "$(readlink -f "$0")")/docker-manual-common.sh
 DOCKER_IMAGE="z3ntu/android-n-build-env"
 BUILD_COMMANDS="""
 export USER=\$(whoami)
+export ALLOW_MISSING_DEPENDENCIES=true
 
 source build/envsetup.sh
 
